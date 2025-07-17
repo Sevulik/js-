7ed8764f70a0db1f4a2483acfdfd760ed69f5912
Sevulik <srcseval@gmail.com>
2025-07-17 18:15:11 +0500

    js normativ 5 ta masala

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
-let number = +prompt('Enter any number: ')
-if (number % 2 == 0){
-  console.log('Juft son');
-}
-else{
-  console.log('Toq');
-  console.log('Toq');
-  console.log('Toq');
-}
+// let number = +prompt('Enter any number: ')
+// if (number % 2 == 0){
+//   console.log('Juft son');
+// }
+// else{
+//   console.log('Toq');
+//   console.log('Toq');
+//   console.log('Toq');
+// }
+
+
+// let number = +prompt("Enter any number: ")
+// if (number > 0){
+//   console.log('positive');
+// }
+// else{
+//   console.log('negative');
+// }
+
+
+// function fn(a,b){
+//   if (a > b){
+//     console.log('first number is bigger');
+//   }
+//   else if(a == b){
+//     console.log("equal");   
+//   }else{
+//     console.log('second number is bigger'); 
+//   }
+// }
+// let a = +prompt("Enter any number: ")
+// let b = +prompt("Enter any number: ")
+// fn()
+
+
+// let score = +prompt('Enter your score: ')
+// if (score <= 59){
+//   console.log('Baho: 2.');
+// } else if (score > 59 && score <= 69){
+//   console.log('Baho: 3');
+// } else if (score >= score && score <= 89){
+//   console.log('Baho: 4');
+// } else if (score >= 90){
+//   console.log('Baho: 5');
+// }
+
+// 5
+// let login = prompt("Enter your login: ")
+// let password = prompt("Enter your password: ")
+// function fn(){
+//   if (login != 'ustudy2025'){
+//     console.log('Wrong login');
+//   } else if(password != 'U9frontend'){
+//     console.log("wrong password");
+//   }
+//   else if(login != 'ustudy2025' && password != 'U9frontend'){
+//     console.log("Wrong login and password");
+//   }else if(login == "ustudy2025" && password=="U9frontend"){
+//     console.log("Sucses");
+    
+//   }
+// }
+
+ 
+// fn()
